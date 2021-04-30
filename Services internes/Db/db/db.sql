f7b81f4472016f3fe09d219bcb398102252ada13
Louis Carlier <63374771+CarlierLouis@users.noreply.github.com>
2021-04-30 11:39:20 +0200

    Update db.sql

---
+++ Services internes/Db/db/db.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE wt_db;
+
+CREATE USER 'admin' IDENTIFIED BY 'admin';
+
+GRANT ALL PRIVILEGES ON wt_db.* TO 'admin'; 
+
+CREATE TABLE jouets (id INTEGER NOT NULL, nom varchar(40) NOT NULL, prix INTEGER NOT NULL);
+
+INSERT INTO jouets (nom, prix)
+VALUE ('toupie', 5), ('train', 20), ('yo-yo', 4);
