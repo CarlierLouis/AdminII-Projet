c42b3a0b70a0122ce718815e598d5e89fac7f1b2
Louis Carlier <63374771+CarlierLouis@users.noreply.github.com>
2021-05-08 18:25:06 +0200

    Update index.php

--- Services internes/Web erp/www/index.php
+++ Services internes/Web erp/www/index.php
@@ -19,7 +19,34 @@
         while ($row = mysqli_fetch_array($affichage)) {
             echo $row['id'] . ': ' . $row['nom'] . ' ' . $row['prix'] . ' <br />';
         }
+        
+        if (isset($_POST['submit'])) {
+     
+        $nom = $_POST["nom"];
+	    $prix = $_POST["prix"]; 
+      
+        $sql = "INSERT INTO jouets (nom, prix) VALUES (?,?)";
+        $stmt= $db->prepare($sql);
+        $stmt->bind_param("si", $nom, $prix);
+        $stmt->execute();
+            
+        } 
+      
+        mysqli_close($db);
+        
     ?>
+      
+      <div style="float:right">
+          <a>Ajouter un objets au catalogue</a><br><br>
+          <form method="POST">  
+              nom:<br>
+              <input type="text" name="nom"><br>
+              prix:<br>
+              <input type="number" name="prix"><br>
+              <br><br>
+              <input type="submit" name="submit" value="Envoyer">
+          </form>
+      </div>
     
   </body>
 
